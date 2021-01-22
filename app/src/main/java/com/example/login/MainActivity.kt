f8d4745f69556098f630310816d7360bbcb0787d
mozx9421 <77826948+mozx9421@users.noreply.github.com>
2021-01-22 14:41:12 +0700

    Work Assignment

---
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toast_fail=Toast.makeText(applicationContext,"Don't use same user and password. ",Toast.LENGTH_LONG)
+        val toast_fail_1=Toast.makeText(applicationContext,"Please enter the same password in both password fields.",Toast.LENGTH_LONG)
+        val toast_pass=Toast.makeText(applicationContext,"Register successful",Toast.LENGTH_LONG)
+
+        ToastButton.setOnClickListener(){
+            if (Username.text.toString() == Password.text.toString()){
+                toast_fail.show()
+            }
+            else{
+                if (Password.text.toString() == Confirm.text.toString()){
+                    toast_pass.show()
+                }
+                else{}
+                toast_fail_1.show()
+            }
+        }
+
+        ResetButton.setOnClickListener(){
+            Username.setText(null)
+            Password.setText(null)
+            Confirm.setText(null)
+        }
+    }
+
+
+}
